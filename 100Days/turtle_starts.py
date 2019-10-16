c6be653189d056c44acaa2bad7a9d9325abc7b99
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-10-16 10:17:12 +0800

    Add files via upload

---
+++ 100Days/turtle_starts.py
@@ -0,0 +1,24 @@
+#from turtle import *
+import turtle as t
+
+#笔刷宽度
+t.width(10)
+
+#t.pencolor('yellow')
+t.forward(200)#前进
+t.right(90)#右转
+
+t.pencolor('red')#笔刷颜色
+t.forward(100)
+t.right(90)
+
+t.pencolor('green')
+t.forward(200)
+t.right(90)
+
+t.pencolor('blue')
+t.forward(100)
+t.right(90)
+
+#调用done()使得窗口等待被关闭,否则将立刻关闭窗口
+t.done()
