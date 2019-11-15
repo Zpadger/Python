2d4188e71a6a9c685252e70dea5bfa6cc692ef0b
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-11-15 11:36:38 +0800

    Update prog.py

--- Argparse/prog.py
+++ Argparse/prog.py
@@ -6,17 +6,15 @@ import argparse
 parser=argparse.ArgumentParser()
 parser.parse_args()
 
-#python prog.py
-# python prog.py --help
+# $ python3 prog.py
+# $ python3 prog.py --help
 # usage: prog.py [-h]
-#
+
 # optional arguments:
 #   -h, --help  show this help message and exit
-
-# python prog.py --verbose
+# $ python3 prog.py --verbose
 # usage: prog.py [-h]
 # prog.py: error: unrecognized arguments: --verbose
-
-# python prog.py foo
+# $ python3 prog.py foo
 # usage: prog.py [-h]
 # prog.py: error: unrecognized arguments: foo
