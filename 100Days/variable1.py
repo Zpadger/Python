20a92ebf00a4b40f7dc4322767ba6e0debc5a6ca
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-12-17 18:00:20 +0800

    Add files via upload

---
+++ 100Days/variable1.py
@@ -0,0 +1,12 @@
+# 使用变量保存数据并进行操作
+
+a = 4
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) #浮点除
+print(a // b) #整除
+print(a % b) #余数
+print(a ** b) #幂
---
+++ 100Days/variable2.py
@@ -0,0 +1,16 @@
+#使用type()检查变量的类型
+
+a = 100 #整型
+b = 1000000000000000000 #
+c = 12.345 #浮点
+d = 1 + 5j #复数
+e = 'A' #字节
+f = 'hello, world'#字符串
+g = True #布尔
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
