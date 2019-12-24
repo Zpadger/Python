9ea5143aa1322a75f212830c716342192d3d49a9
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-12-24 21:16:04 +0800

    新增了按下回车键停止程序运行的功能

--- 100Days/Day07/marquee.py
+++ 100Days/Day07/marquee.py
@@ -1,21 +1,28 @@
 #动态弹幕
 
 import os
-import time
+import _thread, time
+from pip._vendor.distlib.compat import raw_input
+
+def input_thread(L):
+    raw_input()
+    L.append(None)
 
 def main():
+    L = []
+    _thread.start_new_thread(input_thread,(L,))
     str = 'Welcome to Huazhong University of Science and Technology      '
-    counter = 0
+    #counter = 0
     while True:
+        if L : break
         print(str)
-        counter += 1
+        #counter += 1
         time.sleep(0.2)
         str = str[1:] + str[0:1]
         # for Windows use os.system('cls') instead
         os.system('clear')
-        if counter == 100:
-            break
-
+        # if counter == 100:
+        #     break
 
 if __name__ == '__main__':
     main()
