9fb9c71a309a7066f91f4bb0c288a6221f818a4a
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-01-09 17:25:04 +0800

    Update thread_ex3.py

--- 100Days/Day13/thread_ex3.py
+++ 100Days/Day13/thread_ex3.py
@@ -9,11 +9,11 @@ class Account(object):
         self._balance = 0
 
     def deposit(self,money):
-
+        # 计算存款后的余额
         new_balance = self._balance + money
-
+        # 模拟受理存款业务需要0.01秒的时间
         sleep(0.01)
-
+        # 修改账户余额
         self._balance = new_balance
 
     @property
@@ -32,12 +32,12 @@ class AddMoneyThread(Thread):
 def main():
     account = Account()
     threads = []
-
+    # 创建100个存款的线程向同一个账户中存钱
     for _ in range(100):
         t = AddMoneyThread(account,1)
         threads.append(t)
         t.start()
-
+    # 等所有存款的线程都执行完毕
     for t in threads:
         t.join()
     print('账户余额为:$%d元' % account.balance)
