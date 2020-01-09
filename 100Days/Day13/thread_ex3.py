aae63963fbc7c6f9bb4dc7bf653712fb9b789d7b
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-01-09 11:46:15 +0800

    Add files via upload

---
+++ 100Days/Day13/thread_ex3.py
@@ -0,0 +1,51 @@
+# 一个资源被多个线程竞争使用，那么我们通常称之为“临界资源”
+
+from time import sleep
+from threading import Thread
+
+class Account(object):
+
+    def __init__(self):
+        self._balance = 0
+
+    def deposit(self,money):
+
+        new_balance = self._balance + money
+
+        sleep(0.01)
+
+        self._balance = new_balance
+
+    @property
+    def balance(self):
+        return self._balance
+
+class AddMoneyThread(Thread):
+    def __init__(self,account,money):
+        super().__init__()
+        self._account = account
+        self._money = money
+
+    def run(self):
+        self._account.deposit(self._money)
+
+def main():
+    account = Account()
+    threads = []
+
+    for _ in range(100):
+        t = AddMoneyThread(account,1)
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+    print('账户余额为:$%d元' % account.balance)
+
+if __name__ == '__main__':
+    main()
+
+# 运行上面的程序，结果让人大跌眼镜，100个线程分别向账户中转入1元钱
+# 结果居然远远小于100元。之所以出现这种情况是因为我们没有对银行账户这个“临界资源”加以保护
+# 多个线程同时向账户中存钱时，会一起执行到new_balance = self._balance + money这行代码
+# 多个线程得到的账户余额都是初始状态下的0，所以都是0上面做了+1的操作，因此得到了错误的结果
