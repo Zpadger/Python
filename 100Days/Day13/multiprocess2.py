b9220640708712e8de0e87adf25ed34d925161c5
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-01-10 17:41:30 +0800

    Add files via upload

---
+++ 100Days/Day13/multiprocess2.py
@@ -0,0 +1,35 @@
+# 实现进程间的通信
+
+import multiprocessing
+import os
+
+def sub_task1(queue):
+    print('子进程进程号:',os.getpid())
+    counter = 0
+    while counter < 10:
+        queue.put('Pong')
+        counter += 1
+
+def sub_task2(queue):
+    counter = 0
+    while counter < 10:
+        queue.put('Ping')
+        counter += 1
+
+def main():
+    print('当前进程号:', os.getpid())
+    queue = multiprocessing.Queue()
+    p1 = multiprocessing.Process(target=sub_task1, args=(queue,))
+    p1.start()
+    p1.join()
+
+    p2 = multiprocessing.Process(target=sub_task2, args=(queue,))
+    p2.start()
+    p2.join()
+
+    print('子任务已经完成.')
+    for _ in range(20):
+        print(queue.get(), end='')
+
+if __name__ == '__main__':
+    main()
