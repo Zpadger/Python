a9bc861228a1257fbbec27ae06c9f10df0fa851a
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-03-13 11:03:54 +0800

    读取Word文件

---
+++ 100Days/Day15/word.py
@@ -0,0 +1,14 @@
+# 读取Word文件
+
+from docx import Document
+
+doc = Document('用函数还是用复杂的表达式.docx')
+
+print(len(doc.paragraphs))
+print(doc.paragraphs[0].text)
+
+content = []
+for para in doc.paragraphs:
+    content.append(para.text)
+
+print(''.join(content))
