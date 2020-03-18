2ad308cc39037c0ab710a4b8cf0c5f5f7b7434e0
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-03-18 18:19:06 +0800

    折半查找

---
+++ 100Days/Day16/bin_search.py
@@ -0,0 +1,14 @@
+# 折半查找
+
+def bin_search(items, key):
+
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            start = mid - 1
+        else:
+            return mid
+    return -1
