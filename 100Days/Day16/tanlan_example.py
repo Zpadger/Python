443f986fda27a6b76c01b885437b81e77ebb661b
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-03-19 10:22:57 +0800

    贪婪算法

---
+++ 100Days/Day16/tanlan_example.py
@@ -0,0 +1,37 @@
+
+class Thing(object):
+
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    @property
+    def value(self):
+        # 性价比
+        return self.price / self.weight
+
+def input_thing():
+    # 输入物品信息
+    name_str, price_str, weight_str = input().split()
+    return name_str, int(price_str), int(weight_str)
+
+def main():
+    max_weight, num_of_things = map(int, input().split())
+    all_things = []
+    for _ in range(num_of_things):
+        all_things.append(Thing(*input_thing()))
+    all_things.sort(key=lambda  x : x.value, reverse=True)  # 降序
+    total_weight = 0
+    total_price = 0
+    for thing in all_things:
+        if total_weight + thing.weight <= max_weight:
+            print(f'小偷拿走了{thing.name}')
+            total_weight += thing.weight
+            total_price += thing.price
+    print(f'总价值：{total_price}美元')
+    print(f'总重量：{total_weight}公斤')
+
+if __name__ == '__main__':
+    main()
+
