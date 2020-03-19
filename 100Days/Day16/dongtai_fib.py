7650fba7edf036401f3e90690fea94dfdeb01226
Zpadger <31385823+Zpadger@users.noreply.github.com>
2020-03-19 16:29:15 +0800

    动态规划

---
+++ 100Days/Day16/dongtai_fib.py
@@ -0,0 +1,13 @@
+# 动态规划 - 适用于有重叠子问题和最优子结构性质的问题
+# 使用动态规划方法所耗时间往往远少于朴素解法(用空间换取时间)
+
+def fib(num, temp={}):
+
+    # 递归
+    if num in (1, 2):
+        return 1
+    try:
+        return temp[num]
+    except KeyError:
+        temp[num] = fib(num - 1) + fib(num - 2)
+        return temp[num]
